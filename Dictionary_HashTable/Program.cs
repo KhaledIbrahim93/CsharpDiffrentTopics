2e8b1a7f31d80f37f887e34b05884ad7e69bc0be
Khaled Ibrahim <khaledcs@outlook.com>
2019-10-09 17:15:36 +0200

    Add Different bt Dictionary_HashTable

--- Dictionary_HashTable/Program.cs
+++ Dictionary_HashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,26 @@ namespace Dictionary_HashTable
 {
     class Program
     {
+       public class Demo
+        {
+            public Dictionary<int,string> NewDictionary { get; set; }
+            public Hashtable NewHashTable { get; set; }
+        }
+         
         static void Main(string[] args)
         {
+            Demo demo = new Demo();
+            demo.NewDictionary.Add(1, "khaled");
+            demo.NewDictionary.Add(1, "Ibrahim");
+            demo.NewDictionary.Add(1, "Kamel");
+            //A Dictionary is a generic collection..
+            //In Dictionary, you must specify the type of key and value.
+            demo.NewHashTable.Add(1, "khaled");
+            demo.NewHashTable.Add("khaled", "khaled");
+            //In Dictionary, you must specify the type of key and value.
+
+
+
         }
     }
 }
